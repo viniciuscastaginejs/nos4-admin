88e0874b49d6eedbf50de911c22253aa14feeaad
viniciuscastaginejs <viniciuscastagine1@gmail.com>
2026-04-03 11:24:08 -0300

    Switch from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Eventos from './pages/Eventos'
 import NovoEvento from './pages/NovoEvento'
 import EditarEvento from './pages/EditarEvento'
+import TrocarSenha from './pages/TrocarSenha'
+import Usuarios from './pages/Usuarios'
 import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="/dashboard/eventos" element={<PrivateRoute><Eventos /></PrivateRoute>} />
         <Route path="/dashboard/eventos/novo" element={<PrivateRoute><NovoEvento /></PrivateRoute>} />
         <Route path="/dashboard/eventos/editar/:id" element={<PrivateRoute><EditarEvento /></PrivateRoute>} />
+        <Route path="/dashboard/trocar-senha" element={<PrivateRoute><TrocarSenha /></PrivateRoute>} />
+        <Route path="/dashboard/usuarios" element={<PrivateRoute><Usuarios /></PrivateRoute>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
